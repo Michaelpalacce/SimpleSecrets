bc7476a98604daee5ea94d346862c49c2fa2b67b
Stefan Genov <stefantigro@gmail.com>
2022-02-27 18:16:10 +0200

    [main] Start Of integration tests

--- spec/tests/integration/api/simplesecrets/add.spec.ts
+++ spec/tests/integration/api/simplesecrets/add.spec.ts
@@ -31,4 +31,47 @@ describe( "Adding New Secrets", () => {
 		expect( body.inUse ).toBeFalse();
 		expect( typeof body.data ).toBe( "string" );
 	});
+
+	it('should add new secret version if secret exists', async function () {
+		// Variables
+		const name		= "test";
+		const namespace	= "test";
+
+		// Remove If exists
+		await SecretsHelper.ensureSecretIsMissing( name, namespace );
+
+		const firstVersion	= await sendServerRequest( "/api/simplesecrets", "POST", {
+			"namespace": "test",
+			"name": "test",
+			"type": "Opaque",
+			"data": {
+				"user": "testUser",
+				"password": "testPass"
+			}
+		});
+
+		const secondVersion	= await sendServerRequest( "/api/simplesecrets", "POST", {
+			"namespace": "test",
+			"name": "test",
+			"type": "Opaque",
+			"data": {
+				"user": "testUser123",
+				"password": "testPass123"
+			}
+		});
+
+		const firstBody		= JSON.parse( firstVersion.body.toString() );
+		const secondBody	= JSON.parse( secondVersion.body.toString() );
+		expect( firstBody.version ).toBe( 1 );
+		expect( secondBody.version ).toBe( 1 );
+		expect( firstBody.type ).toBe( "Opaque" );
+		expect( secondBody.type ).toBe( "Opaque" );
+		expect( firstBody.name ).toBe( name );
+		expect( secondBody.name ).toBe( name );
+		expect( firstBody.namespace ).toBe( namespace );
+		expect( secondBody.namespace ).toBe( namespace );
+		expect( firstBody.inUse ).toBeFalse();
+		expect( secondBody.inUse ).toBeFalse();
+		expect( firstBody.data ).not.toBe( secondBody.data );
+	});
 });
