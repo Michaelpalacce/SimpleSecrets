cf54357a9bb967ac9c8d93fb3ccc00f0be34766b
Stefan Genov <stefantigro@gmail.com>
2022-02-28 23:10:30 +0200

    [main] Test

--- spec/tests/integration/helpers/setup.helper.ts
+++ spec/tests/integration/helpers/setup.helper.ts
@@ -1,7 +1,9 @@
 import server			from "../../../../api/main/server/server";
 import DatabaseHelpers	from "./DatabaseHelpers";
+import createEncKey from "../../../../api/main/utils/encryption/encryption_key";
 
 beforeAll(async () => {
+	await createEncKey( 'testEncKey' );
 	await server();
 	await DatabaseHelpers.clearDb();
 });
