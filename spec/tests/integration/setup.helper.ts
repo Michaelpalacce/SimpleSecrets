f98f3e18c571c726f37dd51d84af85e8ec5777b7
Stefan Genov <stefantigro@gmail.com>
2022-03-01 01:36:35 +0200

    [main] Tests

--- spec/tests/integration/setup.helper.ts
+++ spec/tests/integration/setup.helper.ts
@@ -1,9 +1,15 @@
 import server			from "../../../api/main/server/server";
 import DatabaseHelpers	from "./helpers/DatabaseHelpers";
+import {apiClient} from "./helpers/clients";
 
 beforeAll(async () => {
 	await server();
 	await DatabaseHelpers.clearDb();
+	await apiClient.createNamespace( { metadata:{ name: "test" } } ).catch( e => {
+		// Don't throw if already exists
+		if ( e.statusCode !== 409 )
+			throw e;
+	});
 });
 
 beforeEach( async () => {
